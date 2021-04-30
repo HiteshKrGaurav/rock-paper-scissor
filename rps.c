3ced9945c15bd59fd647ae7483516506d3c4c034
hitesh-04 <75922163+hitesh-04@users.noreply.github.com>
2021-04-30 15:00:42 +0530

    Update rps.c

--- rps.c
+++ rps.c
@@ -27,7 +27,7 @@ int main()
     int weapon1, weapon2;
     int results;
 
-    char weapon_list[][7] = {{"Rock"}, {"Paper"}, {"Scissor"}};
+    char weapon_list[][8] = {{"Rock"}, {"Paper"}, {"Scissor"}};
 
     char *player1;
     player1 = (char *)malloc(5 * sizeof(char));
@@ -38,41 +38,44 @@ int main()
     scanf("%s", player1);
 
     printf("The game is starting for %s...\n\n", player1);
-    printf("Winning score: %d\n‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n", win_score);
+    printf("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\nWinning score: %d\n", win_score);
 
     // Starting loop
-
     int game_start = 0;
     int game_end = 3;
     while (game_start < game_end)
     {
         game_start++;
+        printf("Turn no. %d\n", game_start);
 
         // If game draws this gives another chance
-        if (game_start == 2)
+        if (game_start == 3)
         {
             if (player_score == cpu_score)
             {
-                printf("The game is draw, hence one more turn is played.\n");
-                game_start = game_start - 1;
+                printf("\nThe game is draw, hence one more turn is played.\n");
+                game_end = game_end + 1;
             }
         }
 
         // Displaying scores
-        printf("\n%s's score: %d\n", player1, player_score);
+        printf("\n‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n");
+
+        printf("%s's score: %d\n", player1, player_score);
         printf("CPU's score: %d\n", cpu_score);
-        printf("\n%s' Turn ==>\n\n", player1);
 
         // weapon selection by player1
-        printf("Enter 1 for Rock, 2 for Scissors & 3 for Paper\n");
-        printf("> ");
+        printf("\n%s's Turn ==>\n\n", player1);
+        printf("Enter 1 for Rock, 2 for Paper & 3 for Scissors\n");
+        printf(": ");
         scanf("%d", &weapon1);
         weapon1 = weapon1 - 1;
-        printf("\n%s chose %s\n", player1, weapon_list[weapon1]);
+        printf("\n=> %s chooses %s\n", player1, weapon_list[weapon1]);
 
         // weapon selection by cpu
+        printf("\nCPU's Turn ==>\n\n", player1);
         weapon2 = randint(3);
-        printf("CPU chose %s\n\n", weapon_list[weapon2]);
+        printf("=> CPU chooses %s\n\n", weapon_list[weapon2]);
 
         // Calculating Conclusion
         results = Conclusion(weapon1, weapon2);
@@ -115,14 +118,11 @@ int main()
     return 0;
 }
 
-
 int randint(int n)
 {
     srand(time(NULL)); // srand takes seed as an input and defined inside stdlib.h
     return rand() % n;
 }
-
-
 
 int Conclusion(int WEAPON1, int WEAPON2)
 {
